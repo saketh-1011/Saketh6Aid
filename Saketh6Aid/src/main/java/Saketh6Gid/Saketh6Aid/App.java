2032d1d002199d8b51a5aa4a91a6d37d06405bbd
saketh1011 <saketh1011@saketh1011-Latitude-7400-2-in-1>
2025-06-01 12:37:55 +0530

    intial commit message

---
+++ Saketh6Aid/src/main/java/Saketh6Gid/Saketh6Aid/App.java
@@ -0,0 +1,13 @@
+package Saketh6Gid.Saketh6Aid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
